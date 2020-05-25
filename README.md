fe8f6922dcf689eee652b6e4687a868acb3b3085
Edilson Rodrigues <58483945+edilson-rodrigues@users.noreply.github.com>
2020-05-25 10:09:18 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+[desafio_em_dart]
+
+montar a metade da arvore de natal 
+
+![](screenshot.png)
+
+com for sem usar numeros no laço de repetição
---
+++ bin/desafio.dart
@@ -0,0 +1,20 @@
+//desafio for #327
+
+//usar o for sem numeros no laço de repetição
+
+
+void main() {
+  //sem numeros
+  for (var valor = '#'; valor != '#######'; valor += '#') {
+    print(valor);
+  }
+  //com numeros
+
+  print('\nacima sem numero e a baixo com numeros\n');
+
+  var valor = '#';
+  for (var i = 0; i < 6; i++) {    
+    print(valor);
+    valor += '#';
+  }
+}
